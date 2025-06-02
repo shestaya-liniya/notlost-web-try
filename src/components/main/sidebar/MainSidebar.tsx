2c520ff1a1e0b5404e8ab91d401fdd64c2b616ef
Shestoy06 <pochta5550@gmail.com>
2025-06-02 13:09:04 +0200

    style: :lipstick: Main - Introduce sidebar
    Just styling: workspace tabs, account

---
+++ src/components/main/sidebar/MainSidebar.tsx
@@ -0,0 +1,39 @@
+import type { FC } from '../../../lib/teact/teact';
+import React, { memo } from '../../../lib/teact/teact';
+import { withGlobal } from '../../../global';
+
+import type { ApiPeer } from '../../../api/types';
+
+import { selectPeer } from '../../../global/selectors';
+
+import Avatar from '../../common/Avatar';
+import WorkspaceSelector from './WorkspaceSelector';
+
+import styles from './MainSidebar.module.scss';
+
+type StateProps = {
+  peer?: ApiPeer;
+};
+
+const MainSidebar: FC<StateProps> = ({
+  peer,
+}) => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.workspaceSelectorsContainer}>
+        <WorkspaceSelector emoji="✨" title="Personal" selected />
+        <WorkspaceSelector emoji="👨‍💻" title="Dev" />
+      </div>
+      <Avatar className={styles.profile} peer={peer} size="xl" />
+    </div>
+  );
+};
+
+export default memo(withGlobal(
+  (global): StateProps => {
+    const peer = selectPeer(global, global.currentUserId || '');
+    return {
+      peer,
+    };
+  },
+)(MainSidebar));
